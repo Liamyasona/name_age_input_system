f822b9ac53f95afbee7ac272efbe4a58394aa12e
unknown <liamkyleyasona@gmail.com>
2024-10-23 00:52:02 +0800

    error message

--- name_age_input_system.py
+++ name_age_input_system.py
@@ -2,12 +2,17 @@ name_age = {}
 
 # Loop 1 is used to ask user for input
 while True:
-    
+    # Loop 2 is retry when user did not input a number
     while True:
         try:
             name = input("Please input your full name: ")
-            age = input("Please input your age: ")
+            age = int(input("Please input your age: "))
             break
 
         except:
             print("Error")
+
+    if error == "n":
+        break
+    elif retry == "y":
+        print("invalid")
